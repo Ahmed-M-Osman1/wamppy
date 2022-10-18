653f2a44badcbbe80927766032698ad0e92a6d16
akiid777 <akiid777@gmail.com>
2022-10-18 22:23:38 +0200

    Add .gitignore file

--- migrations/test/sqls/20221017220109-add-products-table-test-up.sql
+++
@@ -1,7 +0,0 @@
-/* Create products table */
-CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-	price DECIMAL(10,3) NOT NULL,
-	category VARCHAR(100) NOT NULL
-);
